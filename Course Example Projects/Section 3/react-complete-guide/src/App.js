0df878bd21eb7ee7fc29ee3aa2898fb04ebe5942
Your Name <wamontgomery0068@gmail.com>
2019-11-21 11:05:18 -0700

    Demonstration Changes

--- Course Example Projects/Section 3/react-complete-guide/src/App.js
+++ Course Example Projects/Section 3/react-complete-guide/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 
-class App extends Component() {
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Your are viewing the App Component! </h1>
+        <h1> Hakuna Matata </h1>
       </div>
     );
+
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'));
   }
 }
 
