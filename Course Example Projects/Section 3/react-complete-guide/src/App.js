4c960f8444d7a5abbc498291692c6e3b795febd3
Your Name <wamontgomery0068@gmail.com>
2019-12-03 09:54:30 -0700

    Understanding and Using State

--- Course Example Projects/Section 3/react-complete-guide/src/App.js
+++ Course Example Projects/Section 3/react-complete-guide/src/App.js
@@ -5,17 +5,50 @@ import Person from './Person/Person';
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1> This is from the App Component </h1>
-        <Person name = "Andrew" age = "30" />
-        <Person name = "Joel" age = "32" />
-        <Person name = "John" age = "24"> My Hobbies: Watch Alabama Football </Person>
-      </div>
-    );
 
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'));
+  state = {
+
+    persons: [
+
+      { name: 'Andrew', age: 30 },
+
+      { name: 'Joel', age: 35 },
+
+      { name: 'John', age: 24 }
+
+    ]
+
+  }
+
+  // We can define a special property named state.
+  // State is managed from inside a component.
+  // Important, this state property here is only available like this in components that extend components.
+  // So in class-based React components.
+  // We initialize it by assigning a value and this value is a JavaScript object.
+
+  render() {
+
+    return (
+
+      <div className="App">
+
+        <h1> This is from the App Component </h1>
+
+        <p> This is really working!</p>
+        
+        <button> Switch Name </button>
+        {/* Add a button which if we click it, simply switchesone of the names we use here. */}
+        
+        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
+        
+        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} />
+        
+        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} > My Hobbies: Watch Alabama Football </Person>
+      
+      </div>
+    
+    );
+  
   }
 }
 
